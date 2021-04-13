15cd2798a6859127504bb7acc5d64706b63c3a92
shrik <shrik@10.0.0.29>
2021-04-12 22:03:06 -0400

    test step updated

--- cucumber_march/src/test/java/stepDefinition/test_steps.java
+++ cucumber_march/src/test/java/stepDefinition/test_steps.java
@@ -18,7 +18,6 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-
 public class test_steps {
 	public String browserType;
 	public final String screenshotsFolderPath = System.getProperty("user.dir") + "\\output\\screenshots\\";
